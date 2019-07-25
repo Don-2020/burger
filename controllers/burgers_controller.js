327af1618065652b05ea137cfd7df2f0ee26e350
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-25 14:57:34 -0700

    Updated

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+//create all routes and set up logic
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+})
+
+router.get("/api/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [req.body.burger_name, req.body.devoured],
+        function (data) {
+            res.json({ id: data.insertId })
+        })
+})
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condtion, function (result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end()
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+//Delete route
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function (result) {
+        if (result.affectedRows === 0) {
+            return res.status(404).end()
+        } else {
+            res.status(200).end();
+        }
+    })
+});
+
+//Export
+module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
 -- Inserted a set of records into the table
-INSERT INTO cast (burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Bacon Cheeseburger", false);
 
-INSERT INTO cast (burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Double 'N Cheese Steakburger", true);
 
-INSERT INTO cast (burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Veggie Cheeseburger", false);
