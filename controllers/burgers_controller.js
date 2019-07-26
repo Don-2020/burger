0f83edc92bef4cf3f8c4dd759d1e7ed9615dfe37
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-25 22:35:30 -0700

    fixed my routers and post

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,32 +15,31 @@ router.get("/", function (req, res) {
     });
 })
 
-router.get("/api/burgers", function (req, res) {
-    burger.insertOne([
-        "burger_name", "devoured"
-    ], [req.body.burger_name, req.body.devoured],
-        function (data) {
-            console.log("Error right here")
-            res.json({ id: data.insertId })
-        })
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(req.body.burger_name, function() {
+        res.redirect('/')
+    })
 })
 
-router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
+router.post("/api/burgers/:id", function (req, res) {
+    burger.updateOne(req.params.id, function(){
+        res.redirect('/')
+    })
+    // var condition = "id = " + req.params.id;
 
-    console.log("no error is here")
-    console.log("condition", condition);
+    // console.log("no error is here")
+    // console.log("condition", condition);
 
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condtion, function (result) {
-        if (result.changedRows === 0) {
-            console.log("No, no error over heere")
-            return res.status(404).end()
-        } else {
-            res.status(200).end();
-        }
-    });
+    // burger.updateOne({
+    //     devoured: req.body.devoured
+    // }, condtion, function (result) {
+    //     if (result.changedRows === 0) {
+    //         console.log("No, no error over heere")
+    //         return res.status(404).end()
+    //     } else {
+    //         res.status(200).end();
+    //     }
+    // });
 });
 
 //Delete route
